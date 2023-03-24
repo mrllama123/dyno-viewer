7bf5da2c7c97b75ef4b3cac0161aab160a3480c2
Rowan Self <piesrule123@gmail.com>
2023-03-25 12:39:33 +1300

    switch between different tables

--- dyn_cli/app.py
+++ dyn_cli/app.py
@@ -21,29 +21,39 @@ import textual.events as events
 from dyn_cli.aws.sts import get_available_profiles
 import boto3
 import pandas as pd
-from dyn_cli.aws.ddb import scan_items, get_ddb_client
+from dyn_cli.aws.ddb import scan_items, get_ddb_client, get_table_client
 
 
 class DynTable(Widget):
+    data_table = pd.DataFrame()
+
     def compose(self) -> ComposeResult:
         yield DataTable()
 
     async def on_mount(self) -> None:
-        dynamodb_client = boto3.resource("dynamodb").Table("dummy-bank")
-        results, next_token = scan_items(dynamodb_client, paginate=False, Limit=10)
-        df = pd.DataFrame(results)
-
         table = self.query_one(DataTable)
 
-        table.add_columns(*df.columns.tolist())
-        table.add_rows(df.values.tolist())
+        table.add_columns(*self.data_table.columns.tolist())
+        table.add_rows(self.data_table.values.tolist())
+    
+    def change_table_data(self, table_name):
+        dyn_table_client = get_table_client(table_name)
+        table = self.query_one(DataTable)
+        results, next_token = scan_items(dyn_table_client, paginate=False, Limit=10)
+        self.data_table = pd.DataFrame(results)
+        
+        table.clear()
+        table.add_columns(*self.data_table.columns.tolist())
+        table.add_rows(self.data_table.values.tolist())
 
+
+    
 
 
 class TableSelectScreen(Screen):
     BINDINGS = [("escape", "app.pop_screen", "Pop screen")]
 
-    class Selected(Message):
+    class TableName(Message):
         """pass back what table was selected"""
 
         def __init__(self, table: str) -> None:
@@ -59,10 +69,9 @@ class TableSelectScreen(Screen):
             id="dynTablesSelect",
         )
 
-    async def on_list_view_selected(self, item: ListItem) -> None:
-        self.post_message(self.Selected(item.id))
+    async def on_list_view_selected(self, selected) -> None:
+        self.post_message(self.TableName(selected.item.id))
         self.app.pop_screen()
-
 
 
 class DynCli(App):
@@ -77,16 +86,28 @@ class DynCli(App):
 
     aws_profile = reactive("default")
 
+    table_name = reactive("", layout=True)
+
     def compose(self) -> ComposeResult:
         yield Footer()
-        yield Horizontal(DynTable())
+        yield Horizontal(Label(self.table_name) ,DynTable())
 
-    # async def action_profile(self) -> None:
-    #     profile_list = ListView(*[ListItem(Label(profile)) for profile in self.profiles])
-    #     self.mount(profile_list)
+    async def on_table_select_screen_table_name(
+        self, table_name: TableSelectScreen.TableName
+    ) -> None:
+        self.table_name = table_name.table
+
+        # profile_list = ListView(*[ListItem(Label(profile)) for profile in self.profiles])
+        # self.mount(profile_list)
 
     async def action_exit(self) -> None:
         self.app.exit()
+
+    def watch_table_name(self, new_table_name: str) -> None: 
+        if new_table_name != "":
+            table = self.query_one(DynTable)
+            table.change_table_data(new_table_name)
+
 
 
 def main() -> None:
