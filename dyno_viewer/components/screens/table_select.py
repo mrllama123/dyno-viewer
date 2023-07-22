c74e7295a31326c80de705493dde56b71cd8ec7b
Kylos <piesrule123@gmail.com>
2023-07-23 09:42:34 +1200

    add back an table selection list with option list
    was needed as input suggestion was too limited
    
    currently paginating tables doesn't work

--- dyno_viewer/components/screens/table_select.py
+++ dyno_viewer/components/screens/table_select.py
@@ -1,20 +1,14 @@
+from textual import log, on, work
 from textual.app import ComposeResult
-from textual.widgets import (
-    ListItem,
-    ListView,
-    Label,
-)
-from textual.screen import Screen
+from textual.containers import Vertical
 from textual.events import Key
 from textual.message import Message
-from textual.containers import Vertical
-from textual.widgets import Input
-from dyno_viewer.aws.ddb import get_ddb_client, list_all_tables
-from textual import log
 from textual.reactive import reactive
-from textual import work, on
-from textual.suggester import SuggestFromList, SuggestionReady
+from textual.screen import Screen
+from textual.widgets import Input, OptionList
 from textual.worker import get_current_worker
+
+from dyno_viewer.aws.ddb import list_all_tables
 
 
 class TableSelectScreen(Screen):
@@ -42,33 +36,34 @@ class TableSelectScreen(Screen):
     class TableListResult(Message):
         """return result from listing tables"""
 
-        def __init__(self, table_result, next_token) -> None:
+        def __init__(self, table_result, next_token=None) -> None:
             self.table_result = table_result
             self.next_token = next_token
             super().__init__()
 
     def compose(self) -> ComposeResult:
         yield Input(placeholder="Search for table", id="tableSelectInput")
+        yield OptionList(id="optionDdbTableList")
 
     # worker methods
 
     @work(exclusive=True)
-    def update_tables(self, next_token):
+    def worker_list_tables(self, next_token=None):
         worker = get_current_worker()
         if not worker.is_cancelled:
             if next_token:
-                dynamodb_tables, next_token = list_all_tables(
+                list_tables_result, next_token = list_all_tables(
                     self.dyn_client,
                     Limit=10,
                     ExclusiveStartTableName=next_token,
                     paginate=False,
                 )
             else:
-                dynamodb_tables, next_token = list_all_tables(
+                list_tables_result, next_token = list_all_tables(
                     self.dyn_client, Limit=10, paginate=False
                 )
 
-            self.post_message(self.TableListResult(dynamodb_tables, next_token))
+            self.post_message(self.TableListResult(list_tables_result, next_token))
 
     # on methods
 
@@ -76,24 +71,10 @@ class TableSelectScreen(Screen):
         table_input = self.query_one(Input)
         table_input.focus()
 
-
-
     def on_input_submitted(self, submitted: Input.Submitted) -> None:
         if submitted.value in self.tables:
             self.post_message(self.TableName(submitted.value))
             self.app.pop_screen()
-
-    async def on_list_view_selected(self, selected: ListView.Selected) -> None:
-        input = self.query_one(Input)
-        input.value = selected.item.id
-        input.focus()
-
-    @on(SuggestionReady)
-    async def on_table_suggest(self, suggestion_ready: SuggestionReady) -> None:
-
-        if not suggestion_ready.suggestion and self.next_token:
-            self.update_tables(self.next_token)
-            
 
     @on(TableListResult)
     async def on_table_list_result(self, result: TableListResult):
@@ -104,13 +85,25 @@ class TableSelectScreen(Screen):
         ]
         self.tables.extend(new_tables)
         self.next_token = result.next_token
-        self.query_one(Input).suggester = SuggestFromList(
-            self.tables, case_sensitive=False
-        )
+        option_list = self.query_one(OptionList)
+        option_list.add_options(self.tables)
+
+    @on(Input.Changed, "#tableSelectInput")
+    async def on_table_search_changed(self, changed: Input.Changed) -> None:
+        table_list = self.query_one(OptionList)
+        if changed.input.value:
+            matched_tables = [
+                table for table in self.tables if changed.input.value in table
+            ]
+            table_list.clear_options()
+            if matched_tables:
+                table_list.add_options(matched_tables)
+
 
     # watch methods
 
     async def watch_dyn_client(self, new_dyn_client) -> None:
         if new_dyn_client:
-
-            self.update_tables(self.next_token)
+            option_list = self.query_one(OptionList)
+            option_list.clear_options()
+            self.worker_list_tables()
