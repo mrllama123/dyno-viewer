fee8d37b9222b5b0e82b315fe2f20501780ca7ed
Rowan Self <rowan.self@troveplatform.co.nz>
2024-02-26 12:45:56 +1300

    move to separate folder

--- tests/unit/app/test_select_table.py
+++ tests/unit/app/test_select_table.py
@@ -1,20 +1,9 @@
 import pytest
 
 
-@pytest.fixture
-def dyn_cli(mocker, ddb_table_with_data):
-    import boto3
-
-    mocker.patch(
-        "dyno_viewer.app.get_ddb_client", return_value=boto3.client("dynamodb")
-    )
+async def test_select_table(ddb_table_with_data):
     from dyno_viewer.app import DynCli
-
-    return DynCli
-
-
-async def test_select_table(dyn_cli):
-    async with dyn_cli().run_test(size=(100, 50)) as pilot:
+    async with DynCli().run_test(size=(100, 50)) as pilot:
         assert pilot.app.dyn_client
         assert pilot.app.table_name == ""
         await pilot.press("t", "tab", "down", "enter", "enter")
@@ -27,8 +16,9 @@ async def test_select_table(dyn_cli):
         await pilot.press("x")
 
 
-async def test_select_multi_table(dyn_cli):
-    async with dyn_cli().run_test(size=(100, 50)) as pilot:
+async def test_select_multi_table(ddb_table_with_data):
+    from dyno_viewer.app import DynCli
+    async with DynCli().run_test(size=(100, 50)) as pilot:
         assert pilot.app.dyn_client
         assert pilot.app.table_name == ""
         await pilot.press("t", "tab", "down", "enter", "enter")
