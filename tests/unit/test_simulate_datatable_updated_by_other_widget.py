d03f80eb6e90751ebaf0addaae023c5967fde43d
mrllama123 <piesrule123@gmail.com>
2024-05-27 19:52:27 +1200

    added initial pagination support for test example

--- tests/unit/test_simulate_datatable_updated_by_other_widget.py
+++ tests/unit/test_simulate_datatable_updated_by_other_widget.py
@@ -9,17 +9,22 @@ class DataTableManager(Widget):
 
     data = reactive(None)
     static_cols = reactive(["pk", "sk"])
+    page_index = reactive(-1)
 
     def compose(self):
         yield DataTable()
 
-    def watch_data(self, new_data: list[dict] | None):
-        if new_data:
+    def watch_data(self, new_data):
+        if new_data and self.page_index == -1:
+            self.page_index = 0
+
+    def watch_page_index(self, new_page: int):
+        if new_page != -1 and self.data:
             table = self.query_one(DataTable)
             table.clear(columns=True)
             non_static_cols = {
                 col
-                for data_item in new_data
+                for data_item in self.data[self.page_index]
                 for col in data_item.keys()
                 if col not in self.static_cols
             }
@@ -27,12 +32,10 @@ class DataTableManager(Widget):
             for col in cols:
                 table.add_column(col, key=col)
             rows = [
-                [item.get(col) for col in cols]
-                for item in new_data
+                [item.get(col) for col in cols] for item in self.data[self.page_index]
             ]
 
             table.add_rows(rows)
-
 
 
 class DataTableManagerApp(App):
@@ -44,33 +47,38 @@ class DataTableManagerApp(App):
 
 async def test_data_table_manager():
     data = [
-        {"pk": "customer#12345", "sk": "CUSTOMER"},
-        {"pk": "customer#54321", "sk": "CUSTOMER"},
+        [
+            {"pk": "customer#12345", "sk": "CUSTOMER"},
+            {"pk": "customer#54321", "sk": "CUSTOMER"},
+        ]
     ]
     app = DataTableManagerApp()
     async with app.run_test() as pilot:
         pilot.app.data = data
         await pilot.pause()
         table = app.query_one(DataTable)
-        assert table.row_count == len(data)
-        table_rows = [table.get_row_at(i) for i in range(0, len(data))]
+        assert table.row_count == len(data[0])
+        table_rows = [table.get_row_at(i) for i in range(0, len(data[0]))]
         assert table_rows == [
             ["customer#12345", "CUSTOMER"],
             ["customer#54321", "CUSTOMER"],
         ]
 
+
 async def test_data_table_manager_extra_data():
     data = [
-        {"pk": "customer#12345", "sk": "CUSTOMER", "testAttr": "test"},
-        {"pk": "customer#54321", "sk": "CUSTOMER", "testAttr": "test"},
+        [
+            {"pk": "customer#12345", "sk": "CUSTOMER", "testAttr": "test"},
+            {"pk": "customer#54321", "sk": "CUSTOMER", "testAttr": "test"},
+        ]
     ]
     app = DataTableManagerApp()
     async with app.run_test() as pilot:
         pilot.app.data = data
         await pilot.pause()
         table = app.query_one(DataTable)
-        assert table.row_count == len(data)
-        table_rows = [table.get_row_at(i) for i in range(0, len(data))]
+        assert table.row_count == len(data[0])
+        table_rows = [table.get_row_at(i) for i in range(0, len(data[0]))]
         assert table_rows == [
             ["customer#12345", "CUSTOMER", "test"],
             ["customer#54321", "CUSTOMER", "test"],
