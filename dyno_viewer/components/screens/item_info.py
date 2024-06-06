9b8b8498ff5f69d7e663eb529ec13cd04a5d1bc0
mrllama123 <piesrule123@gmail.com>
2024-06-06 23:02:08 +1200

    simplify item viewer to static widget

--- dyno_viewer/components/screens/item_info.py
+++ dyno_viewer/components/screens/item_info.py
@@ -1,5 +1,8 @@
+from decimal import Decimal
 from textual.screen import ModalScreen
-from textual.widgets import Pretty
+from textual.widgets import Static
+import json
+from rich.json import JSON
 
 
 class ItemInfo(ModalScreen):
@@ -12,14 +15,18 @@ class ItemInfo(ModalScreen):
         classes: str | None = None,
         item: dict | None = None,
     ) -> None:
-        self.item_payload = item
         super().__init__(name, id, classes)
+        self.item_payload = item
+
+    def convert_decimal_to_float(self, value):
+        if isinstance(value, Decimal):
+            return float(value)
+        return value
+
+    def format_dict(self, item):
+        return {k: self.convert_decimal_to_float(v) for k, v in item.items()}
 
     def compose(self):
-
-        yield Pretty(self.item_payload)
-
-    def watch_item_payload(self, new_payload):
-        if new_payload:
-            pretty = self.query_one(Pretty)
-            pretty.update(new_payload)
+        formatted_item = self.format_dict(self.item_payload)
+        json_str = json.dumps(formatted_item)
+        yield Static(JSON(json_str))
