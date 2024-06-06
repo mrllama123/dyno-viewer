25f376d9279acdd06c6d4c171a5d8f8fe3666186
mrllama123 <piesrule123@gmail.com>
2024-06-06 21:21:26 +1200

    basic support for viewing item row as json object

--- dyno_viewer/components/screens/item_info.py
+++ dyno_viewer/components/screens/item_info.py
@@ -1,10 +1,19 @@
 from textual.screen import ModalScreen
-from textual.reactive import reactive
 from textual.widgets import Pretty
+
 
 class ItemInfo(ModalScreen):
     BINDINGS = {("escape", "app.pop_screen", "exit")}
-    item_payload = reactive({})
+
+    def __init__(
+        self,
+        name: str | None = None,
+        id: str | None = None,
+        classes: str | None = None,
+        item: dict | None = None,
+    ) -> None:
+        self.item_payload = item
+        super().__init__(name, id, classes)
 
     def compose(self):
 
@@ -14,4 +23,3 @@ class ItemInfo(ModalScreen):
         if new_payload:
             pretty = self.query_one(Pretty)
             pretty.update(new_payload)
-
